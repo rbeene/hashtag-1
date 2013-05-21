3d3d35a5198ba38657674dcce7ef75722a207983
Robert Beene <rbeene@teladoc.com>
2013-05-21 13:22:30 -0400

    fix stuff

--- spec/features/guest_searches_for_a_hashtag_spec.rb
+++ spec/features/guest_searches_for_a_hashtag_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 feature 'Guest searches for a hashtag' do
   scenario 'displays search results' do
     visit root_path
-    fill_in 'Search', with: '#rails'
-    click_on 'Submit'
+    search_for("#rails")
 
     expect(current_path).to eq '/searches/rails'
     expect(page).to have_css 'li', text: /#rails/i, count: 15
@@ -12,8 +11,7 @@ feature 'Guest searches for a hashtag' do
 
   scenario 'searches without a hashtag' do
     visit root_path
-    fill_in 'Search', with: 'rails'
-    click_on 'Submit'
+    search_for("rails")
 
     expect(current_path).to eq '/searches/rails'
     expect(page).to have_css 'li', text: /#rails/i, count: 15
@@ -21,13 +19,14 @@ feature 'Guest searches for a hashtag' do
 
   scenario 'updates search results' do
     visit root_path
-    fill_in 'Search', with: '#rails'
-    click_on 'Submit'
-
-    fill_in 'Search', with: '#ruby'
-    click_on 'Submit'
+    search_for("#ruby")
 
     expect(current_path).to eq '/searches/ruby'
     expect(page).to have_css 'li', text: /#ruby/i, count: 15
+  end
+
+  def search_for(term)
+    fill_in 'Search', with: term
+    click_on 'Submit'
   end
 end
